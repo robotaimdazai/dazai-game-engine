3f6cdd9d3f7097a5c7746b484935bad46c47fba2
Bilal Azhar <github.com>
2023-06-25 06:09:25 +0500

    Fixed collision bug

--- src/engine/ecs/systems/system_collision_detection.cpp
+++ src/engine/ecs/systems/system_collision_detection.cpp
@@ -49,28 +49,38 @@ auto system_collision_detection::fixed_update(float fixed_delta_time) -> void
                 }
             }
         }
-        //now sort all the colliders
         
+        if(selected_rigidbody == nullptr) //dont do all this when no rigid body is found
+            continue;
+        
+        //now sort all the colliders
         std::ranges::sort(sorted_entities,[](const std::pair<::entity,float>& a,const std::pair<::entity,float>& b)
         {
             return a.second<b.second;
         });
 
-        for (auto const& entity:sorted_entities)
+        //iterate through all sorted colliders and accumulate net velocity
+        glm::vec2 final_velocity{0,0};
+        for (auto const& pair:sorted_entities)
         {
-            auto other_rect = system_collider_renderer::get_collider_rect(entity.first);
+            auto other_rect = system_collider_renderer::get_collider_rect(pair.first);
             if(selected_rigidbody!=nullptr)
             {
                 auto rigidbody =*selected_rigidbody;
                 if(physics2d::dynamic_rect_intersects_rect(rigidbody.velocity,selected_rect,other_rect,contact_point,
                     contact_normal,contact_time))
                 {
-                    rigidbody.velocity += m_collision_resolution_factor_ * contact_normal *
+                   final_velocity +=  contact_normal *
                         glm::vec2(std::abs(rigidbody.velocity.x),std::abs(rigidbody.velocity.y)) * (1-contact_time);
-                    glm::vec3 velocity = {rigidbody.velocity.x,rigidbody.velocity.y,0};
-                    selected_transform.position+=velocity ;
                 } 
             }
+        }
+        if(selected_rigidbody!=nullptr)
+        {
+            auto rigidbody =*selected_rigidbody;
+            rigidbody.velocity = final_velocity;
+            glm::vec3 velocity = {rigidbody.velocity.x,rigidbody.velocity.y,0};
+            selected_transform.position+=velocity ;
         }
     }
 }
--- src/engine/ecs/systems/system_collision_detection.h
+++ src/engine/ecs/systems/system_collision_detection.h
@@ -6,7 +6,4 @@ class system_collision_detection:public system
 public:
     auto fixed_update(float fixed_delta_time)->void;
     auto debug_draw()->void;
-
-private:
-    const float m_collision_resolution_factor_= 1.01f; // this adds 1 percent more velocity just to keep the object out of the collider
 };
